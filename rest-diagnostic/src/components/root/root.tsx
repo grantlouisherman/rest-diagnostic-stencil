2fccae2c0a1a9a01fb8f086c22a8ea366161a7c2
grantlouisherman <grantlouisherman041@gmail.com>
2020-02-07 11:42:00 -0800

    updating fetch request and adding reset button

--- rest-diagnostic/src/components/root/root.tsx
+++ rest-diagnostic/src/components/root/root.tsx
@@ -1,6 +1,6 @@
 import { Component, h, Listen, State } from "@stencil/core";
 import yaml from 'js-yaml'
-
+import { shouldConstructFetchRequest } from '../../utils/utils'
 
 @Component({
   tag: "app-root",
@@ -9,32 +9,36 @@ import yaml from 'js-yaml'
 })
 export class Root {
   @State() fileContents: any;
+  @State() diagnosticCompleted: boolean = false;
 
   @Listen('upoadCompleteEvent')
-  uploadFileHanlder(event: CustomEvent){
+  uploadFileHanlder(event: CustomEvent) {
     let FILE_CONTENTS = yaml.load(event.detail);
     FILE_CONTENTS = FILE_CONTENTS.calls;
-    for(let key in FILE_CONTENTS){
+    for (let key in FILE_CONTENTS) {
       FILE_CONTENTS[key].fileId = key
     }
     this.fileContents = FILE_CONTENTS;
   }
 
   @Listen('formChanged')
-  formChangeHanlder(event: CustomEvent){
-    const { id, value} = event.detail;
-    const lookUpValues:string  = id.split('-');
+  formChangeHanlder(event: CustomEvent) {
+    const { id, value } = event.detail;
+    const lookUpValues: string = id.split('-');
     const lookUpId: string = lookUpValues[0];
     const lookUpType = lookUpValues[1];
     this.fileContents[lookUpId][lookUpType] = value;
-    console.log(this.fileContents);
   }
 
-  renderFileUpload(){
+  renderFileUpload() {
     return <file-upload></file-upload>
   }
 
-  downLoadHandler(){
+  downLoadHandler() {
+    if (!this.diagnosticCompleted) {
+      alert("You still need to run your calls");
+      return;
+    }
     const content = JSON.stringify(this.fileContents, undefined, 2);
     const BLOB = new Blob([content], { type: "text/plain;charset=utf-8" });
     const url = window.URL || window.webkitURL;
@@ -47,20 +51,56 @@ export class Root {
     document.body.removeChild(a);
   }
 
-  renderDiagnosticItems(){
+  async testCalls() {
+    Promise.all(this.fileContents.map(shouldConstructFetchRequest))
+      .then((calls: any) => {
+        for (let index in calls) {
+          const currCall = calls[index];
+          const callIndex = currCall.fileId;
+          const fileContent = this.fileContents[callIndex];
+          fileContent.response = currCall;
+        }
+      })
+      .then(() => {
+        this.diagnosticCompleted = true;
+      })
+      .catch(err => console.log(err))
+  }
+
+  Reset(){
+    this.fileContents = null;
+  }
+  renderButtons() {
+    return [
+      <button class="ui primary basic button"
+      onClick={this.Reset.bind(this)}>
+      Reset
+      </button>,
+      <button class="ui primary basic button"
+        onClick={this.downLoadHandler.bind(this)}>
+        Download Report
+        </button>,
+      <button class="ui primary basic button"
+        onClick={this.testCalls.bind(this)}
+      >
+        Run Calls
+      </button>
+    ]
+  }
+  renderDiagnosticItems() {
     const calls = this.fileContents.map(call => (
       <diagnose-item {...call}></diagnose-item>
     ));
-    return [ 
-      ...calls,
-      <button class="ui primary basic button" onClick={this.downLoadHandler.bind(this)}>Download Report</button>
+    return [
+      this.renderButtons(),
+      ...calls
     ]
   }
 
   render() {
     return (
       <div class="ui container">
-        { this.fileContents ? this.renderDiagnosticItems() : this.renderFileUpload() }
+        {this.fileContents ? this.renderDiagnosticItems() : this.renderFileUpload()}
       </div>
     );
   }
--- rest-diagnostic/src/utils/utils.ts
+++ rest-diagnostic/src/utils/utils.ts
@@ -1,1 +1,29 @@
 
+
+export const shouldConstructFetchRequest = async (fetchInfo: any) => {
+  // fileId:number, method: string, headers: any, query: any, url: string
+  const { method, headers, url } = fetchInfo
+  const query = method != 'GET' ? fetchInfo.query : null
+  const body = method != 'GET' ? JSON.stringify({ query }) : null
+  let fetchRequest;
+  let fetchRequestJson;
+  try {
+    fetchRequest = await fetch(url, {
+      method,
+      mode: 'cors',
+      headers,
+      body
+    })
+    fetchRequestJson = await fetchRequest.json()
+  } catch (err) {
+    console.error(new Error(`Fetch Call failed ${err}`));
+    fetchRequestJson = err
+  }
+
+  return {
+    status: fetchRequest.status,
+    responseBody: fetchRequestJson,
+    fileId: fetchInfo.fileId
+  }
+}
+
