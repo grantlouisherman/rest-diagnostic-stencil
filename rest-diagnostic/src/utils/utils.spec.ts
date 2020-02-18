1e5953100047c4a0196a24aecc38906be1a08cde
grantlouisherman <grantlouisherman041@gmail.com>
2020-02-17 19:37:52 -0800

    feature(utils): all tests passing utils

--- rest-diagnostic/src/utils/utils.spec.ts
+++ rest-diagnostic/src/utils/utils.spec.ts
@@ -1,21 +1,60 @@
-import { format } from './utils';
-
-describe('format', () => {
-  it('returns empty string for no names defined', () => {
-    expect(format(undefined, undefined, undefined)).toEqual('');
+import { shouldConstructFetchRequest, iconResolver } from './utils';
+//const { method, headers, url, checkbox } = fetchInfo
+describe('shouldConstructFetchRequest', () => {
+  it('should return data that has body', async () => {
+    let data = await shouldConstructFetchRequest({
+      fileId: 0,
+      method:'GET', 
+      headers: {}, 
+      url:'https://jsonplaceholder.typicode.com/posts/1',
+      checkbox: true
+    })
+    expect(data).toEqual({
+      status: 404,
+      responseBody: {
+        data: "Not Found"
+      },
+        fileId: 0
+    });
   });
-
-  it('formats just first names', () => {
-    expect(format('Joseph', undefined, undefined)).toEqual('Joseph');
+  it('should return data that has no body', async () => {
+    let data = await shouldConstructFetchRequest({
+      fileId: 0,
+      method:'GET', 
+      headers: {}, 
+      url:'https://jsonplaceholder.typicode.com/posts/1',
+      checkbox: false
+    })
+    expect(data).toEqual({
+      status: 404,
+      responseBody: 'You opted to not include the body. Click on the checkbox to include it',
+      fileId: 0
+    });
   });
-
-  it('formats first and last names', () => {
-    expect(format('Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
-  });
-
-  it('formats first, middle and last names', () => {
-    expect(format('Joseph', 'Quincy', 'Publique')).toEqual(
-      'Joseph Quincy Publique'
-    );
+  it('should return an error', async () => {
+    let data = await shouldConstructFetchRequest({
+      fileId: 0,
+      method:'GET', 
+      headers: {}, 
+      url:null,
+      checkbox: false
+    })
+    expect(data).toEqual({
+      status: 500,
+      responseBody: "There was an error: Error: missing url input for mock fetch()",
+      fileId: 0
+    });
   });
 });
+
+describe('iconResolver', () => {
+  it('should return teal color', () => {
+    expect(iconResolver(200)).toEqual('teal');
+  });
+  it('should return yellow color:404', () => {
+    expect(iconResolver(404)).toEqual('yellow');
+  });
+  it('should return yellow color:500', () => {
+    expect(iconResolver(500)).toEqual('yellow');
+  });
+})
